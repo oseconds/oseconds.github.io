b53a4128bb13d28516ac967ad11d73057419a02f
OH YEONGSEO <123317581+oseconds@users.noreply.github.com>
2026-08-12 16:32:14 +0900

    metadata

--- src/lib/canvas/data/videos.ts
+++ src/lib/canvas/data/videos.ts
@@ -18,7 +18,9 @@ export const videos: VideoSource[] = [
 		title: 'video source test',
 		artist: '0seconds',
 		video: '/media/Leftfield-Bass.mp4',
-		defaultBpm: 100,
+		bpm: 100,
+		bpmConfidence: 0.8,
+		beats: [0, 2, 4, 6],
 		recommendedBars: 3,
 		tags: ['leftfield', 'loop']
 	},
@@ -27,7 +29,9 @@ export const videos: VideoSource[] = [
 		title: 'second video house',
 		artist: '0seconds',
 		video: '/media/house_1.mp4',
-		defaultBpm: 128,
+		bpm: 128,
+		bpmConfidence: 0.9,
+		beats: [0, 4, 8, 12],
 		recommendedBars: 4,
 		tags: ['house','four-on-the-floor', 'loop']
 	}
