7f73e6e12cd34fadcbf390862d58b373c09fa580
OH YEONGSEO <123317581+oseconds@users.noreply.github.com>
2026-08-18 16:46:01 +0900

    Update soldVideos.ts

--- src/lib/canvas/data/soldVideos.ts
+++ src/lib/canvas/data/soldVideos.ts
@@ -1,60 +1,60 @@
-import type { VideoSource } from './types';
+// import type { VideoSource } from './types';
 
-export const soldVideos: VideoSource[] = [
-	{
-		id: 'Shake-It-Hot_1',
-		title: 'Crash Up',
-		artist: 'Loxxey',
-		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
-		video: '/media/SHAKEITHOT/CRASH-UP_slp.mp4',
-		bpm: 140,
-		recommendedBars: 4,
-		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
-		spotifyUrl: 'https://open.spotify.com/track/1ZVQVBPdqhW2o1cduKTgry?si=44bae19e45b44e18'
-	},
-	{
-		id: 'Shake-It-Hot_2',
-		title: 'Shake It Hot',
-		artist: 'Loxxey',
-		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
-		video: '/media/SHAKEITHOT/SIH_slp.mp4',
-		bpm: 140,
-		recommendedBars: 4,
-		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
-		spotifyUrl: 'https://open.spotify.com/track/0FegU3srBP2gi8SAsN9f6z?si=db97ef2f8c3242c9'
-	},
-	{
-		id: 'Shake-It-Hot_3',
-		title: 'No Action',
-		artist: 'Loxxey',
-		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
-		video: '/media/SHAKEITHOT/NO_ACTION_prob4.mp4',
-		bpm: 140,
-		recommendedBars: 4,
-		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
-		spotifyUrl: 'https://open.spotify.com/track/6PSS94NtECYYeSleDtnzq9?si=c76cfc1194d34ba5'
-	},
+// export const soldVideos: VideoSource[] = [
+// 	{
+// 		id: 'Shake-It-Hot_1',
+// 		title: 'Crash Up',
+// 		artist: 'Loxxey',
+// 		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
+// 		video: '/media/SHAKEITHOT/CRASH-UP_slp.mp4',
+// 		bpm: 140,
+// 		recommendedBars: 4,
+// 		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
+// 		spotifyUrl: 'https://open.spotify.com/track/1ZVQVBPdqhW2o1cduKTgry?si=44bae19e45b44e18'
+// 	},
+// 	{
+// 		id: 'Shake-It-Hot_2',
+// 		title: 'Shake It Hot',
+// 		artist: 'Loxxey',
+// 		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
+// 		video: '/media/SHAKEITHOT/SIH_slp.mp4',
+// 		bpm: 140,
+// 		recommendedBars: 4,
+// 		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
+// 		spotifyUrl: 'https://open.spotify.com/track/0FegU3srBP2gi8SAsN9f6z?si=db97ef2f8c3242c9'
+// 	},
+// 	{
+// 		id: 'Shake-It-Hot_3',
+// 		title: 'No Action',
+// 		artist: 'Loxxey',
+// 		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
+// 		video: '/media/SHAKEITHOT/NO_ACTION_prob4.mp4',
+// 		bpm: 140,
+// 		recommendedBars: 4,
+// 		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
+// 		spotifyUrl: 'https://open.spotify.com/track/6PSS94NtECYYeSleDtnzq9?si=c76cfc1194d34ba5'
+// 	},
 
-	{
-		id: 'Shake-It-Hot_4',
-		title: 'Hyper',
-		artist: 'Loxxey',
-		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
-		video: '/media/SHAKEITHOT/HYPER_prob4.mp4',
-		bpm: 142,
-		recommendedBars: 4,
-		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
-		spotifyUrl: 'https://open.spotify.com/track/2Ldp3ovLIN7AIwvcnLegoU?si=decb1b887aaf45ed'
-	},
-	{
-		id: 'Shake-It-Hot_5',
-		title: 'DPWM',
-		artist: 'Loxxey',
-		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
-		video: '/media/SHAKEITHOT/DPWM_slp.mp4',
-		bpm: 140,
-		recommendedBars: 4,
-		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
-		spotifyUrl: 'https://open.spotify.com/track/76fK60jvhkrGtrzHUVGBDF?si=39fce8ff031e493b'
-	}
-];
+// 	{
+// 		id: 'Shake-It-Hot_4',
+// 		title: 'Hyper',
+// 		artist: 'Loxxey',
+// 		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
+// 		video: '/media/SHAKEITHOT/HYPER_prob4.mp4',
+// 		bpm: 142,
+// 		recommendedBars: 4,
+// 		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
+// 		spotifyUrl: 'https://open.spotify.com/track/2Ldp3ovLIN7AIwvcnLegoU?si=decb1b887aaf45ed'
+// 	},
+// 	{
+// 		id: 'Shake-It-Hot_5',
+// 		title: 'DPWM',
+// 		artist: 'Loxxey',
+// 		artwork: '/media/Shake-It-Hot/SHAKE-IT-HOT.jpg',
+// 		video: '/media/SHAKEITHOT/DPWM_slp.mp4',
+// 		bpm: 140,
+// 		recommendedBars: 4,
+// 		tags: ['SHAKE-IT-HOT', 'Loxxey', 'bass-music', 'Dubstep', 'PREP-Records'],
+// 		spotifyUrl: 'https://open.spotify.com/track/76fK60jvhkrGtrzHUVGBDF?si=39fce8ff031e493b'
+// 	}
+// ];
